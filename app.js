16d6c497de09a612f58e211661f05dfa25330d2d
Taek-D <95740164+Taek-D@users.noreply.github.com>
2021-12-08 16:22:57 +0900

    Calculator

---
+++ app.js
@@ -0,0 +1,19 @@
+const calculator = {
+    add: function(a, b) {
+        console.log(a+b);
+    },
+    minus: function(a, b) {
+        console.log(a-b);
+    },
+    mul: function(a, b) {
+        console.log(a*b);
+    },
+    div: function(a,b) {
+        console.log(a/b);
+    }
+};
+
+calculator.add(2,3);
+calculator.minus(4,2);
+calculator.mul(5,1);
+calculator.div(200,5);
